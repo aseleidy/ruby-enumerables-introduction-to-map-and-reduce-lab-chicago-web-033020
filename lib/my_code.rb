a76049a24b35fff88e150ad6e7dd65dd5309c045
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-26 02:21:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,11 +45,11 @@ def map_to_square(input)
   new_map
 end 
 
-def reduce_to_total(source_array)
+def reduce_to_total(source)
   total = 0 
   i = 0 
-  while i < source_array.length do 
-    total += source_array[i]
+  while i < source.length do 
+    total += source[i]
     
     i += 1 
   end 
