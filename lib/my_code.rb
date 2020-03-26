bbddac98b5d960d3bf20a4a5cc8e00c1b52c7ca9
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-26 02:06:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,11 @@
 # My Code here....
 def map_to_negativize(array)
-  
-  
+  new_array = []
+  i = 0 
+  while i < array.length do 
+    neg_num = array[i] * (-1)  
+    new_array << neg_num
+    
+    i += 1 
+  end 
 end
