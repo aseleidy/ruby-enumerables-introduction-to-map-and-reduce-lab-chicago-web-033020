ed219a5fa160fa3c052eb558f0a1077b05a9835d
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-26 02:10:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,4 +10,13 @@ def map_to_negativize(array)
   new_array 
 end
 
-def 
+def map_to_no_change(input)
+  new_map = []
+  i = 0 
+  while i < input.length do 
+    new_map << input[i]
+    
+    i += 1
+  end 
+  new_map
+end 
